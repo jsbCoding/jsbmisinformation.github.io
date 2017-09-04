64592ac9568a86544525ab6e98ed46ce372b09d1
jsbmisinformation <josephsalzmanbye@gmail.com>
2017-09-04 20:35:48 +1000

    finished website concept

--- IFFIEwebsite/assets/js/main.js
+++ IFFIEwebsite/assets/js/main.js
@@ -13,7 +13,27 @@ $('.menu').mouseleave(function(){
   $('.menuExpandedWindow').removeClass('show')
 });
 
+/* ----------------------------------------event hovering */
 
+
+$('.homeEvent.homeEventA').mouseover(function(){
+  $('.homeEventA').addClass('darken')
+});
+$('.homeEvent.homeEventA').mouseleave(function(){
+  $('.homeEventA').removeClass('darken')
+});
+$('.homeEvent.homeEventB').mouseover(function(){
+  $('.homeEventB').addClass('darken')
+});
+$('.homeEvent.homeEventB').mouseleave(function(){
+  $('.homeEventB').removeClass('darken')
+});
+$('.homeEvent.homeEventC').mouseover(function(){
+  $('.homeEventC').addClass('darken')
+});
+$('.homeEvent.homeEventC').mouseleave(function(){
+  $('.homeEventC').removeClass('darken')
+});
 
 
 
