fd3b660262b312049c2c74faeeaa3b7bdd7b1b7e
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-09-04 14:12:52 +1000

    developing homepage

--- IFFIEwebsite/assets/js/main.js
+++ IFFIEwebsite/assets/js/main.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+/* ----------------------------------------menu */
+
+
+$('.menu').mouseover(function(){
+  $('.menuExpandedWindow').addClass('show')
+});
+$('.menu').mouseleave(function(){
+  $('.menuExpandedWindow').removeClass('show')
+});
 
 
 
