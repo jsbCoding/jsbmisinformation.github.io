bf9e186d4bcc075bf2b805364d1f968a48d1d4b4
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-10-13 16:25:20 +1100

    settling website design

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
 
 // Add jQuery here
 
-
+$( function() {
+  $( ".policee" ).draggable({ handle: "p" });
+  $( ".police" ).draggable({ cancel: "p.ui-widget-header" });
+  $( "div, p" ).disableSelection();
+} );
 
 
 
