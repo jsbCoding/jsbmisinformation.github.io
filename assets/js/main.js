9453bff5980e6e1fa84dac0b8b02b05b0b7e6356
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-10-17 11:53:34 +1100

    updated icons, news and report pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
 
 // Add jQuery here
 
-$( function() {
-  $( ".policee" ).draggable({ handle: "p" });
-  $( ".police" ).draggable({ cancel: "p.ui-widget-header" });
-  $( "div, p" ).disableSelection();
-} );
-
 
 
 
