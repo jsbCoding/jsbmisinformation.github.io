04219c2a847f358dcb68deb71e312a4a6e586c75
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-10-27 11:59:56 +1100

    newsindex

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 // Add jQuery here
 
 
-
+$('.content .readMoreButton').click(function(){
+  $('.boundWindow.reportWindowA').removeClass('hide');
+  $('.boundWindow.reportWindowA').addClass('show');
+});
 
 
 
