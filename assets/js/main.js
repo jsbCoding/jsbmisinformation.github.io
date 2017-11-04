9963a32441d9abc81b6df2f3eca6758d83ab02d8
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-11-04 19:31:45 +1100

    l

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,33 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+//REPORTS///////////
 
 $('.content .readMoreButton').click(function(){
   $('.boundWindow.reportWindowA').removeClass('hide');
   $('.boundWindow.reportWindowA').addClass('show');
 });
 
+//BOARD MEMBERS///////
+
+$('.boundWindow.boardMember1').click(function(){
+  $('.boardMember1 .blue.boardMemberInfo').toggleClass('hide');
+});
+$('.boundWindow.boardMember2').click(function(){
+  $('.boardMember2 .blue.boardMemberInfo').toggleClass('hide');
+});
+$('.boundWindow.boardMember3').click(function(){
+  $('.boardMember3 .blue.boardMemberInfo').toggleClass('hide');
+});
+$('.boundWindow.boardMember4').click(function(){
+  $('.boardMember4 .blue.boardMemberInfo').toggleClass('hide');
+});
+$('.boundWindow.boardMember5').click(function(){
+  $('.boardMember5 .blue.boardMemberInfo').toggleClass('hide');
+});
+$('.boundWindow.boardMember6').click(function(){
+  $('.boardMember6 .blue.boardMemberInfo').toggleClass('hide');
+})
 
 
 
